0b9fa91c13813a51732bbad00f7f453e37dfd352
MichaelOrlov <o7j665k2sqld66cyfu4m4ato6gbn4h74z5thn6obgladxyckolda>
2022-06-11 18:52:14 +0300

    android {   ...   defaultConfig {     ...     testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"   } }
    dependencies {
        testImplementation 'junit:junit:4.12'
    
        androidTestImplementation 'androidx.test:runner:1.2.0'
        androidTestImplementation 'androidx.test.espresso:espresso-core:3.2.0'

--- android/app/src/main/java/co/appbrewery/flash_chat/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/flash_chat/MainActivity.java
@@ -1,7 +1,15 @@
 package co.appbrewery.flash_chat;
 
 import io.flutter.embedding.android.FlutterActivity;
+import androidx.test.rule.ActivityTestRule;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+// Import MainActivity from YOUR package
+import com.resocoder.integration_testing_prep.MainActivity;
 
-public class MainActivity extends FlutterActivity {
-
+@RunWith(FlutterTestRunner.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
 }
