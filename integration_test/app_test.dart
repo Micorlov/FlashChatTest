82353e60b8089aef6cae076e8529d06898ba174c
MichaelOrlov <o7j665k2sqld66cyfu4m4ato6gbn4h74z5thn6obgladxyckolda>
2022-06-11 18:28:20 +0300

    android {   ...   defaultConfig {     ...     testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"   } }
    estWidgets(
        "After inputting a text, go to the display page which contains that same text "
            "and then navigate back to the typing page where the input should be clear",
            (WidgetTester tester) async {
          await tester.pumpWidget(FlashChat());

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,66 +1,43 @@
+import 'package:flash_chat/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:flash_chat/main.dart' as app;
 
-
 // flutter drive --driver=test_driver/integration_test.dart --target=integration_test/app_test.dart -d R5CR90XZ9QZ --screenshot=c:\a\test\new
 
-
-
 void main() {
-  loginTest();
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets(
+    "After inputting a text, go to the display page which contains that same text "
+        "and then navigate back to the typing page where the input should be clear",
+        (WidgetTester tester) async {
+      await tester.pumpWidget(FlashChat());
+
+      // Input this text
+      final inputText = 'Hello there, this is an input.';
+      await tester.enterText(find.byKey(Key('your-text-field')), inputText);
+
+      // Tap on a FAB
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+
+      // We should be in the DisplayPage that displays the inputted text
+
+      expect(find.text(inputText), findsOneWidget);
+
+      // Tap on the back arrow in the AppBar
+      await tester.tap(find.byType(BackButton));
+      await tester.pumpAndSettle();
+
+      // We should be back in the TypingPage and the previously inputted text
+      // should be cleared out
+
+      expect(find.text(inputText), findsNothing);
+    },
+  );
+
+
 }
-
-Future<void> loginTest() async {
-
-  group('Login test', () {
-
-    setUp(() {
-      print('setUpAll');
-    });
-
-    tearDown(() async {
-      print('tearingDown');
-    });
-
-    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-    testWidgets(" Login test ", (tester) async {
-
-
-
-
-      app.main();
-
-      IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-
-
-      //await tester.pumpAndSettle();
-
-
-      await tester.pump(Duration(seconds: 5));
-
-      final LogIn = find.byKey(Key("LogIn"));
-
-      await tester.tap(LogIn);
-
-      await tester.pump(Duration(seconds: 5));
-
-      final emailField = find.byKey(Key("emailField"));
-
-      await tester.tap(emailField);
-
-      await tester.enterText(emailField, "micorlov@gmail.com");
-
-      await tester.pump(Duration(seconds: 5));
-
-      expect(find.byKey(Key("emailField")), findsOneWidget);
-
-      print('---> done login test');
-
-    });
-  });
-}
-
